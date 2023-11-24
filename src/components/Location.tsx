3d63e02735cc041abec2f77e8f7718269ccf6b4c
Nguyễn Văn Ngọc <ngoc.nguyen6@ntq-solution.com.vn>
2023-11-24 14:51:22 +0900

    commit

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -53,6 +53,9 @@ const Location = ({ config }: LocationProps) => {
           <b>Nhà gái: </b> Số 5 - Ngách 58/1 đường Đình Xuyên - Huyện Gia Lâm- Thành phố Hà Nội
           <br />
           <br />
+          <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d465.3622132562445!2d105.93629049301485!3d21.07674746137864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1zU-G7kSA1IE5nw7UgNTgsIMSQw6xuaCBYdXnDqm4sIEdpYSBMw6JtLCBIw6AgTuG7mWksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1700804158227!5m2!1svi!2s"
+                  loading="lazy" width='97%' height='400'
+                  referrerPolicy="no-referrer-when-downgrade"></iframe>
         </SubTitle>
       </Layout>
     </Section>
