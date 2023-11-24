7d235211463b8b6e46725f2a7109901397964b11
Viet Hung <hung.nguyen5@ntq-solution.com.vn>
2023-11-24 20:21:15 +0900

    commit

--- src/configs.ts
+++ src/configs.ts
@@ -1,11 +1,20 @@
 import TitleImage from './resources/Title.png';
 import LocationMapImage from './resources/LocationMap.png';
-import GalleryPhoto1 from './resources/Gallery_Photo_1.png';
-import GalleryPhoto2 from './resources/Gallery_Photo_2.png';
-import GalleryPhoto3 from './resources/Gallery_Photo_3.png';
-import GalleryPhoto4 from './resources/Gallery_Photo_4.png';
-import GalleryPhoto5 from './resources/Gallery_Photo_5.png';
-import GalleryPhoto6 from './resources/Gallery_Photo_6.png';
+import GalleryPhoto1 from './resources/Image_1.jpg';
+import GalleryPhoto2 from './resources/Image_2.jpg';
+import GalleryPhoto3 from './resources/Image_3.jpg';
+import GalleryPhoto4 from './resources/Image_4.jpg';
+import GalleryPhoto5 from './resources/Image_5.jpg';
+import GalleryPhoto6 from './resources/Image_6.jpg';
+import GalleryPhoto7 from './resources/Image_7.jpg';
+import GalleryPhoto8 from './resources/Image_8.jpg';
+import GalleryPhoto9 from './resources/Image_9.jpg';
+import GalleryPhoto10 from './resources/Image_10.jpg';
+import GalleryPhoto11 from './resources/Image_11.jpg';
+import GalleryPhoto12 from './resources/Image_12.jpg';
+import GalleryPhoto13 from './resources/Image_13.jpg';
+import GalleryPhoto14 from './resources/Image_14.jpg';
+import GalleryPhoto15 from './resources/Image_15.jpg';
 
 const Configs: ConfigsType = {
   weddingDate: 'Ngày 02 tháng 12 năm 2023',
@@ -35,6 +44,15 @@ const Configs: ConfigsType = {
     GalleryPhoto4,
     GalleryPhoto5,
     GalleryPhoto6,
+    GalleryPhoto7,
+    GalleryPhoto8,
+    GalleryPhoto9,
+    GalleryPhoto10,
+    GalleryPhoto11,
+    GalleryPhoto12,
+    GalleryPhoto13,
+    GalleryPhoto14,
+    GalleryPhoto15
   ],
 };
 
